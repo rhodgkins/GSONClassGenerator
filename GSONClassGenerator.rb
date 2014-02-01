a6721cb248c565f959b0d9a496eb56caf87d9006
Rich Hodgkins <rhodgkins@gmail.com>
2014-02-01 18:43:19 +0000

    Added #! to top of ruby file

--- GSONClassGenerator.rb
+++ GSONClassGenerator.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'json'
 
 module GSONClassGenerator
@@ -443,6 +445,12 @@ Flags:
 	f - All fields are final, this will also generate a public no arguement constructor for all classes and the 's' flag is ignored.
 	c - A public constructor is generated for all the classes with all the fields as arguments.
 	b - All primative types are boxed.
+	
+Arguments:
+
+    json_file - The file you want to generate the model class from.
+    class_name - The top level class name, and therefore the name of the output file.
+    output_directory - The location to save the generated model class. If this is not provided the current directory will be used.
 			HELP
 			abort
 		end
